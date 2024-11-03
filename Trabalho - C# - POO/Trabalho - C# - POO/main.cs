34fe58101e11477831f56be9f7bb8f7ecefce1fd
FelipeTolentino21 <161332663+FelipeTolentino21@users.noreply.github.com>
2024-11-03 01:05:46 -0300

    Deletada a tentativa de colocar o trabalho de POO aqui

--- Trabalho - C# - POO/Trabalho - C# - POO/main.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace trabalhoRPG{
-    class rpg1{
-        static void Main(){
-            arqueiro1 jonas = new arqueiro1();
-            Console.WriteLine(jonas.vida);
-        }
-    }
-}
